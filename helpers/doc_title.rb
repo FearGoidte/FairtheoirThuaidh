ac19a35b2fc8587dc3cfd1ed2d3316f2e8956f69
FearGoidte <FearGoidte@users.noreply.github.com>
2016-06-06 20:56:12 +0100

    Added doc_title helper and changed language to use settings

---
+++ helpers/doc_title.rb
@@ -0,0 +1,14 @@
+def doc_title
+  seperator = '—'
+  site_name = data.settings.site.name
+  if @title
+    page_title = @title
+  elsif current_resource.data.title
+    page_title = current_resource.data.title
+  end
+  if page_title
+    page_title << seperator << site_name
+  else
+    return site_name
+  end
+end
